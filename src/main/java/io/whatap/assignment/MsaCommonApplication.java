ab684e6b2ce00165db58fb725aff99de824ffc18
INSU <tonuto@naver.com>
2024-01-10 12:30:16 +0900

    2024.01.10 commit

---
+++ src/main/java/io/whatap/assignment/MsaCommonApplication.java
@@ -0,0 +1,13 @@
+package io.whatap.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCommonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsaCommonApplication.class, args);
+  }
+
+}
---
+++ src/main/java/io/whatap/assignment/cmm/aop/ExcutionTimeType.java
@@ -0,0 +1,23 @@
+package io.whatap.assignment.cmm.aop;
+
+import java.util.function.Function;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StopWatch;
+
+@RequiredArgsConstructor
+@Getter
+public enum ExcutionTimeType {
+
+  MS("ms", "밀리세컨드", StopWatch::getTotalTimeMillis),
+  SECOND("sec", "세컨드", s -> {
+    return Double.valueOf(s.getTotalTimeSeconds()).longValue();
+  }),
+  ;
+  private final String code;
+
+  private final String desc;
+
+  private final Function<StopWatch, Long> time;
+
+}
